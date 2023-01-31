1008a19e1fad35e255c1494abfd09154e1d0dbee
Jaqueline <jaquezubieta@gmail.com>
2023-01-31 14:23:09 +0100

    Change projects pics

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Home from './components/pages/Home';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
-
-
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
