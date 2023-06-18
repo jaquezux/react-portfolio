99a15d43bd64fb85d7e1f3311ba0c1e0b1e35e85
Jaqueline <jaquezubieta@gmail.com>
2023-06-18 17:39:26 +0200

    experience link

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from '../Hero';
 import Experience from '../Experience';
 
 
 
 function Home() {
+    useEffect(() => {
+        if(window.location.search.includes("experience")) {
+            const element = document.querySelector("#experience");
+            element.scrollIntoView({ behavior: 'smooth'});
+        }
+    }, [])
+
     return (
         <>
             <Hero />
