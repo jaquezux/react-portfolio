7651c0c04514608a4e5cc9fb7a3b943abad1ad8a
Jaqueline <jaquezubieta@gmail.com>
2023-09-19 15:31:39 +0200

    about-me-update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,7 +14,7 @@ function Hero() {
 
                 <div className='hero-text'>
                     <h1>Jaqueline Bianco</h1>
-                    <h2>Product Data & Insights 🎮</h2>
+                    <h2>Marketing | Data Analyis | Product Management</h2>
                     <p>Hello there! </p>
 
                     <p>I'm Jaqueline, passionate about Data Analysis & Product.</p>
@@ -23,9 +23,7 @@ function Hero() {
 
                     <p>I challenged myself to develop my portfolio website and here we are! I built this website from scratch, using ReactJS, HTML and CSS. It was important for me to get familiarized with coding, getting into logic mindset and understanding semantics.</p>
 
-                    <p>At the moment, I'm getting specialized in the <strong>Gaming Industry</strong>. My goal is to use my experience to contribute to this market and help to build a safe space to minority groups. </p>
-
-                    <p><strong><a href='https://www.linkedin.com/in/jaquelinebianco/'>LinkedIn</a> | <a href='mailto:jaquezubieta@gmail.com'>E-mail</a> | <a href='https://www.exophase.com/user/jaquezux/'>My Gaming Achievements</a></strong></p>
+                    <p><strong><a href='https://www.linkedin.com/in/jaquelinebianco/'>LinkedIn</a> | <a href='mailto:jaquezubieta@gmail.com'>E-mail</a></strong></p>
 
                 </div>
             </div>
